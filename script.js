3df3a150720327ff8017a2973332338ebe7a7af0
Shane Bruno <72577268+sbruno1519@users.noreply.github.com>
2021-05-09 18:47:38 -0500

    script.js

--- script.js
+++ script.js
@@ -1,359 +1,151 @@
-//Regular Row Variables
-var RegularHead = document.getElementById("regular-polygon-head");
-var RegularTriangleBox = document.getElementById("reg-triangle-box");
-var RegularQuadrillateralBox = document.getElementById("reg-quadrillateral-box");
-var RegularPentagonBox = document.getElementById("reg-pentagon-box");
-var RegularHexagonBox = document.getElementById("reg-hexagon-box");
-var RegularHeptagonBox = document.getElementById("reg-heptagon-box");
-var RegularOctagonBox = document.getElementById("reg-octagon-box");
-//Irregular Row Variables
-var IrregularHead = document.getElementById("irregular-polygon-head");
-var IrregularTriangleBox = document.getElementById("irregular-triangle-box");
-var IrregularQuadrillateralBox = document.getElementById("irregular-quadrillateral-box");
-var IrregularPentagonBox = document.getElementById("irregular-pentagon-box");
-var IrregularHexagonBox = document.getElementById("irregular-hexagon-box");
-var IrregularHeptagonBox = document.getElementById("irregular-heptagon-box");
-var IrregularOctagonBox = document.getElementById("irregular-octagon-box");
-//Types of Polygon Row
-var TypesPolygonHead = document.getElementById("type-polygon-head");
-var ConcaveBox = document.getElementById("concave-box");
-var ConvexBox = document.getElementById("convex-box");
-var ComplexBox = document.getElementById("complex-box");
-//Not Polygon Row
-var NonPolygonHead = document.getElementById("non-polygon-head");
-var CircleBox = document.getElementById("circle-box");
-var CurvesBox = document.getElementById("curves-box");
-var UnclosedShapesBox = document.getElementById("unclosed-shapes-box");
-var ThreeDimensionalObjectsBox = document.getElementById("three-dimensional-objects-box");
-//SVG
-var RegularTriangleSVG = document.getElementById("regular-triangle");
-var TriangleInfo = document.querySelectorAll(".triangle-info");
-//Labels for Items
-var NumberOfSides = document.querySelector(".number-of-sides");
-var ShapeLabel = document.querySelector(".shape-label");
+var items = document.querySelectorAll(".items");
+var svg = document.getElementsByClassName("svg"); 
+var info = document.getElementsByClassName("info");
+var heads = document.getElementsByClassName("heads");
 
-//GSAP
-const tl = gsap.timeline();
- tl
-    .to(RegularTriangleBox, {duration: 1, height:200}, 0)
-    .to(RegularTriangleBox, {duration: 1, width:200}, 0)
-    .to(RegularTriangleSVG, {duration: 1, width: 290}, 1)
-    .to(RegularTriangleSVG, {duration: 1, margin: 40}, 0)
-    .to(RegularTriangleSVG, {duration: 2, attr:{viewBox:"140 -3 90 90"}}, 2)
-    .to(TriangleInfo, {duration: 1, attr:{x:"140"}}, 3);
-tl.pause();
+var infoArray = [
+  /*Regular Triangle*/[info[0], info[1]],
+  /*Regular Rectangle*/[info[2], info[3]],
+  /*Regular Pentagon*/[info[4], info[5]],
+  /*Regular Hexagon*/[info[6], info[7]],
+  /*Regular Heptagon*/[info[8], info[9]],
+  /*Regular Ocatagon*/[info[10], info[11]],
+  /*Irregular Triangle*/[info[12], info[13]],
+  /*Irregular Rectangle*/[info[14], info[15]],
+  /*Irregular Pentagon*/[info[16], info[17]],
+  /*Irregular Hexagon*/[info[18], info[19]],
+  /*Irregular Heptagon*/[info[20], info[21]],
+  /*Irregular Octagon*/[info[22], info[23]],
+  /*Concave*/[info[24], info[25]],
+  /*Convex*/[info[26], info[27]],
+  /*Complex*/[info[28], info[29]],
+  /*Circle*/[info[30], info[31]],
+  /*Shapes with Curves*/[info[32], info[33]],
+  /*Shapes not Closed*/[info[34], info[35]],
+  /*3d Shapes*/[info[36], info[37]],
+  ];
 
-
-
-RegularTriangleBox.onclick = function(){ 
-   
-  if(RegularTriangleBox.style.height = "120px"){     
- tl.play()  
-  } else {  
- tl.reverse()
-  }
-}
-
-RegularQuadrillateralBox.onclick = function(){
+function returnSVG(i){
+  //Original SVG Viewbox Values
+  var originalViewboxValues = [/*Regular Triangle*/"35 -3 90 90",
+           /*Regular Rectangle*/"45 5 90 90",
+           /*Regular Pentagon*/"47 7 65 65",
+           /*Regular Hexagon*/"42 4 75 75",
+           /*Regular Heptagon*/"45 8 70 70", 
+           /*Regular Octogon*/"40 0 80 80", 
+           /*Irregular Triangle*/"40 0 80 80",
+           /*Irregular Rectangle*/"38 0 82 82",
+           /*Irregular Pentagon*/"40 5 80 80",
+           /*Irregular Hexagon*/"40 5 80 80",
+           /*Irregular Heptagon*/"40 2 80 80",
+           /*Irregular Octagon*/"40 0 80 80",
+           /*Concave Polygon*/"35 5 90 90",
+           /*Convex Polygon*/"30 5 90 90",
+           /*Complex*/"35 8 85 85",
+           /*Circle*/"37 42 85 85",
+           /*Shapes with Curves*/"40 5 80 80",
+           /*Shapes Not Closed*/"30 0 100 100",
+           /*3d Shapes*/"0 0 171 171"];
   
-  if(RegularQuadrillateralBox.style.height == "120px"){
-    //Grow box
-    RegularQuadrillateralBox.style.height = "300px";
-    RegularQuadrillateralBox.style.width = "300px";
-    
-    //Add Text
-    //increase size of svg
-  } else{
-    //bring box back to normal properties
-    RegularQuadrillateralBox.style.height = "120px";
-    RegularQuadrillateralBox.style.width = "";
-    RegularQuadrillateralBox.style.flexGrow = "1";
-  }
+  var moveBackSVG = gsap.timeline();
+  moveBackSVG
+    .to(svg[i], {duration: 1, attr:{viewBox: originalViewboxValues[i]}}, .2)
+    .to(infoArray[i], {duration: 1, opacity: "0%"}, .1); 
+  moveBackSVG.play();
 }
 
-RegularPentagonBox.onclick = function(){
+var deactivateItem = function(){
+   for(var i = 0; i < items.length; i++) (function(i){
+     items[i].style.height = "120px";
+     items[i].style.width = "";
+     svg[i].style.margin = "1px"; 
+     returnSVG(i);
+   })(i);
+}
+
+items.forEach((item) => {
+  item.addEventListener("mouseleave", deactivateItem);
+});
+
+//Item Hover
+function changeSVGViewbox(i){
+  //New Viebox values
+  const newViewboxValues = [
+     /*Regular Triangle*/"135 -3 90 90",
+     /*Regular Rectangle*/"145 5 90 90",
+     /*Regular Pentagon*/"147 7 65 65",
+     /*Regular Hexagon*/"142 4 75 75",
+     /*Regular Heptagon*/"145 8 70 70", 
+     /*Regular Octogon*/"140 0 80 80", 
+     /*Irregular Triangle*/"140 0 80 80",
+     /*Irregular Rectangle*/"138 0 82 82",
+     /*Irregular Pentagon*/"140 5 80 80",
+     /*Irregular Hexagon*/"140 5 80 80",
+     /*Irregular Heptagon*/"140 2 80 80",
+     /*Irregular Octagon*/"140 0 80 80",
+     /*Concave Polygon*/"155 5 90 90",
+     /*Convex Polygon*/"150 5 90 90",
+     /*Complex*/"155 8 85 85",
+     /*Circle*/"137 37 85 85",
+     /*Shapes with Curves*/"140 5 80 80",
+     /*Shapes Not Closed*/"130 0 100 100",
+     /*3d Shapes*/"200 0 171 171"
+  ]; 
   
-   if(RegularPentagonBox.style.height == "120px"){
-    //Grow box
-    RegularPentagonBox.style.height = "300px";
-    RegularPentagonBox.style.width = "300px";
-    //Add Text
-    //increase size of svg
-  } else{
-    //bring box back to normal properties
-    RegularPentagonBox.style.height = "120px";
-    RegularPentagonBox.style.width = "";
-    RegularPentagonBox.style.flexGrow = "1";
-  }
+  var slideSVG = gsap.timeline();
+  slideSVG
+     .to(svg[i], {duration: 1,  attr:{viewBox: newViewboxValues[i]}}, .5)
+     .to(infoArray[i], {duration: 1, opacity: "100%"}, 1);
+  slideSVG.play();
+ 
+  items.forEach((item) => {
+    item.addEventListener("mouseleave", function(){
+      slideSVG.pause();
+    });
+  });
 }
 
-RegularHexagonBox.onclick = function(){
-  
-  if(RegularHexagonBox.style.height == "120px"){
-    //Grow box
-    RegularHexagonBox.style.height = "300px";
-    RegularHexagonBox.style.width = "300px";
-    //Add Text
-    //increase size of svg
-  } else{
-    //bring box back to normal properties
-    RegularHexagonBox.style.height = "120px";
-    RegularHexagonBox.style.width = "";
-    RegularHexagonBox.style.flexGrow = "1";
-  }
-}
+//Items Click Event
+for(var a = 0; a < items.length; a++) 
+  (function(a){
+    items[a].onclick = function(){
+      items[a].style.height = "200px";
+      items[a].style.width = "200px";
+      svg[a].style.margin = "40px";
+      changeSVGViewbox(a);   
+    };
+})(a);
+ 
+ 
+const regularRow = document.getElementsByClassName("regular-items");
+const irregularRow = document.getElementsByClassName("irregular-items");
+const typePolygonRow = document.getElementsByClassName("type-items");
+const nonPolygonRow = document.getElementsByClassName("non-polygon-items");
 
-RegularHeptagonBox.onclick = function(){
-  
-  if(RegularHeptagonBox.style.height == "120px"){
-    //Grow box
-    RegularHeptagonBox.style.height = "300px";
-    RegularHeptagonBox.style.width = "300px";
-    //Add Text
-    //increase size of svg
-  } else{
-    //bring box back to normal properties
-    RegularHeptagonBox.style.height = "120px";
-    RegularHeptagonBox.style.width = "";
-    RegularHeptagonBox.style.flexGrow = "1";
-  }
-}
+const regularSVG = document.getElementsByClassName("regular-svg");
+const irregularSVG = document.getElementsByClassName("irregular-svg");
+const typesPolygonSVG = document.getElementsByClassName("types-polygon-svg");
+const nonPolygonSVG = document.getElementsByClassName("non-polygon-svg");
 
-RegularOctagonBox.onclick = function(){
-  
-   if(RegularOctagonBox.style.height == "120px"){
-    //Grow box
-    RegularOctagonBox.style.height = "300px";
-    RegularOctagonBox.style.width = "300px";
-    //Add Text
-    //increase size of svg
-  } else{
-    //bring box back to normal properties
-    RegularOctagonBox.style.height = "120px";
-    RegularOctagonBox.style.width = "";
-    RegularOctagonBox.style.flexGrow = "1";
-  }
-}
+var arrayOfRows = [
+    [regularRow, regularSVG, heads[0]],
+    [irregularRow, irregularSVG, heads[1]], 
+    [typePolygonRow, typesPolygonSVG, heads[2]],
+    [nonPolygonRow, nonPolygonSVG, heads[3]]
+];
 
-//Irregular Polygons
-
-IrregularTriangleBox.onclick = function(){
-  
-   if(IrregularTriangleBox.style.height == "120px"){
-    //Grow box
-    IrregularTriangleBox.style.height = "300px";
-    IrregularTriangleBox.style.width = "300px";
-    //Add Text
-    //increase size of svg
-  } else{
-    //bring box back to normal properties
-    IrregularTriangleBox.style.height = "120px";
-    IrregularTriangleBox.style.width = "";
-    IrregularTriangleBox.style.flexGrow = "1";
-  }
-}
-
-IrregularQuadrillateralBox.onclick = function(){
-  
-  if(IrregularQuadrillateralBox.style.height == "120px"){
-    //Grow box
-    IrregularQuadrillateralBox.style.height = "300px";
-    IrregularQuadrillateralBox.style.width = "300px";
-    //Add Text
-    //increase size of svg
-  } else{
-    //bring box back to normal properties
-    IrregularQuadrillateralBox.style.height = "120px";
-    IrregularQuadrillateralBox.style.width = "";
-    IrregularQuadrillateralBox.style.flexGrow = "1";
-  }
-}
-
-IrregularPentagonBox.onclick = function(){
-  
-  if(IrregularPentagonBox.style.height == "120px"){
-    //Grow box
-    IrregularPentagonBox.style.height = "300px";
-    IrregularPentagonBox.style.width = "300px";
-    //Add Text
-    //increase size of svg
-  } else{
-    //bring box back to normal properties
-    IrregularPentagonBox.style.height = "120px";
-    IrregularPentagonBox.style.width = "";
-    IrregularPentagonBox.style.flexGrow = "1";
-  }
-}
-
-IrregularHexagonBox.onclick = function(){
-  
- if(IrregularHexagonBox.style.height == "120px"){
-    //Grow box
-    IrregularHexagonBox.style.height = "300px";
-    IrregularHexagonBox.style.width = "300px";
-    //Add Text
-    //increase size of svg
-  } else{
-    //bring box back to normal properties
-    IrregularHexagonBox.style.height = "120px";
-    IrregularHexagonBox.style.width = "";
-    IrregularHexagonBox.style.flexGrow = "1";
-  }
-}
-
-IrregularHeptagonBox.onclick = function(){
-  
-  if(IrregularHeptagonBox.style.height == "120px"){
-    //Grow box
-    IrregularHeptagonBox.style.height = "300px";
-    IrregularHeptagonBox.style.width = "300px";
-    //Add Text
-    //increase size of svg
-  } else{
-    //bring box back to normal properties
-    IrregularHeptagonBox.style.height = "120px";
-    IrregularHeptagonBox.style.width = "";
-    IrregularHeptagonBox.style.flexGrow = "1";
-  }
-}
-
-IrregularOctagonBox.onclick = function(){
-  
-  if(IrregularOctagonBox.style.height == "120px"){
-    //Grow box
-    IrregularOctagonBox.style.height = "300px";
-    IrregularOctagonBox.style.width = "300px";
-    //Add Text
-    //increase size of svg
-  } else{
-    //bring box back to normal properties
-    IrregularOctagonBox.style.height = "120px";
-    IrregularOctagonBox.style.width = "";
-    IrregularOctagonBox.style.flexGrow = "1";
-  }
-}
-
-
-ConcaveBox.onclick = function(){
-  
-  if(ConcaveBox.style.height == "120px"){
-   //Grow box
-    ConcaveBox.style.height = "300px";
-    ConcaveBox.style.width = "300px";
-    //Add Text
-    //increase size of svg
-  } else{
-    //bring box back to normal properties
-    ConcaveBox.style.height = "120px";
-    ConcaveBox.style.width = "";
-    ConcaveBox.style.flexGrow = "1";
-  }
-}
-
-ConvexBox.onclick = function(){
-  
-    if(ConvexBox.style.height == "120px"){
-   //Grow box
-    ConvexBox.style.height = "300px";
-    ConvexBox.style.width = "300px";
-    //Add Text
-    //increase size of svg
-  } else{
-    //bring box back to normal properties
-    ConvexBox.style.height = "120px";
-    ConvexBox.style.width = "";
-    ConvexBox.style.flexGrow = "1";
-  }
-}
-
-ComplexBox.onclick = function(){
-  
-    if(ComplexBox.style.height == "120px"){
-   //Grow box
-    ComplexBox.style.height = "300px";
-    ComplexBox.style.width = "300px";
-    //Add Text
-    //increase size of svg
-  } else{
-    //bring box back to normal properties
-    ComplexBox.style.height = "120px";
-    ComplexBox.style.width = "";
-    ComplexBox.style.flexGrow = "1";
-  }
-}
-
-CircleBox.onclick = function(){
-  
-    if(CircleBox.style.height == "120px"){
-   //Grow box
-    CircleBox.style.height = "300px";
-    CircleBox.style.width = "300px";
-    //Add Text
-    //increase size of svg
-  } else{
-    //bring box back to normal properties
-    CircleBox.style.height = "120px";
-    CircleBox.style.width = "";
-    CircleBox.style.flexGrow = "1";
-  }
-}
-
-CurvesBox.onclick = function(){
-  
-    if(CurvesBox.style.height == "120px"){
-   //Grow box
-    CurvesBox.style.height = "300px";
-    CurvesBox.style.width = "300px";
-    //Add Text
-    //increase size of svg
-  } else{
-    //bring box back to normal properties
-    CurvesBox.style.height = "120px";
-    CurvesBox.style.width = "";
-    CurvesBox.style.flexGrow = "1";
-  }
-}
-
-UnclosedShapesBox.onclick = function(){
-  
-    if(UnclosedShapesBox.style.height == "120px"){
-   //Grow box
-    UnclosedShapesBox.style.height = "300px";
-    UnclosedShapesBox.style.width = "300px";
-    //Add Text
-    //increase size of svg
-  } else{
-    //bring box back to normal properties
-    UnclosedShapesBox.style.height = "120px";
-    UnclosedShapesBox.style.width = "";
-    UnclosedShapesBox.style.flexGrow = "1";
-  }
-}
-
-ThreeDimensionalObjectsBox.onclick = function(){
-  
-    if(ThreeDimensionalObjectsBox.style.height == "120px"){
-   //Grow box
-    ThreeDimensionalObjectsBox.style.height = "300px";
-    ThreeDimensionalObjectsBox.style.width = "300px";
-    //Add Text
-    //increase size of svg
-  } else{
-    //bring box back to normal properties
-    ThreeDimensionalObjectsBox.style.height = "120px";
-    ThreeDimensionalObjectsBox.style.width = "";
-    ThreeDimensionalObjectsBox.style.flexGrow = "1";
-  }
-}
-
-/*NumberOfSides.innerHTML = "3 sides triangle";
-    ShapeLabel.style.opacity = "0";
-    
-    //GSAP animation
-    tl.to(RegularTriangleBox, {duration: 1, height:200}, 0);
-    tl.to(RegularTriangleBox, {duration: 1, width:200}, 0);
-    tl.to(RegularTriangleSVG, {duration: 1, width: 290}, 1);
-    tl.to(RegularTriangleSVG, {duration: 1, margin: 40}, 0);
-    tl.to(RegularTriangleSVG, {duration: 2, attr:{viewBox:"140 -3 90 90"}}, 2);
-    tl.to(TriangleInfo, {duration: 1, attr:{x:"140"}}, 3);
-*/
+//Head Hover Effect
+Array.prototype.forEach.call(arrayOfRows, function(rowParts) {
+  rowParts[2].addEventListener('mouseenter', function(){
+      for(var a = 0; a < rowParts[0].length; a++) {
+        rowParts[0][a].style.height = "150px";
+        rowParts[1][a].style.margin = "15px";
+      }
+  });
+  rowParts[2].addEventListener('mouseleave', function(){
+      for(var a = 0; a < rowParts[0].length; a++) {
+        rowParts[0][a].style.height = "120px";
+        rowParts[1][a].style.margin = "1px";
+      }
+  });
+});
